6f78d5fec1d0192d56f3078e2b673bd24b1a39c6
Carl Peters <31207893+cpjjpeters@users.noreply.github.com>
2025-01-05 15:58:38 +0100

    setting up the packages

---
+++ src/main/java/ipeters/capital/persistence/BlahPersistenceFacade.java
@@ -0,0 +1,4 @@
+package ipeters.capital.persistence;
+
+public interface BlahPersistenceFacade {
+}
---
+++ src/test/java/ipeters/capital/CapitalApplicationTests.java
@@ -0,0 +1,13 @@
+package ipeters.capital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapitalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
