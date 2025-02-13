7dd79bc0b5687df2ef2c249ed0626413230e286a
Carl Peters <31207893+cpjjpeters@users.noreply.github.com>
2025-02-13 18:21:08 +0100

    basic empty classes

---
+++ src/main/java/ipeters/capital/controller/CPOrderController.java
@@ -0,0 +1,5 @@
+package ipeters.capital.controller;
+
+/* carlpeters created on 13/02/2025 inside the package - ipeters.capital.controller */
+public class CPOrderController {
+}
---
+++ src/main/java/ipeters/capital/model/CPProgram.java
@@ -0,0 +1,5 @@
+package ipeters.capital.model;
+
+/* carlpeters created on 13/02/2025 inside the package - ipeters.capital.model */
+public class CPProgram {
+}
