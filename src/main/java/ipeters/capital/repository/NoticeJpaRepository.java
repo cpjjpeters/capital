0b99a4700ee30ae44e28979bf500183a05d511e3
Carl Peters <31207893+cpjjpeters@users.noreply.github.com>
2025-02-19 10:42:02 +0100

    works without Notice

--- src/main/java/ipeters/capital/repository/NoticeJpaRepository.java
+++
@@ -1,7 +0,0 @@
-package ipeters.capital.repository;
-
-import ipeters.capital.entity.NoticeJpaEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface NoticeJpaRepository  extends JpaRepository<NoticeJpaEntity, Long> {
-}
--- src/main/java/ipeters/capital/service/EmployeeService.java
+++
@@ -1,5 +0,0 @@
-package ipeters.capital.service;
-
-/* carlpeters created on 08/01/2025 inside the package - ipeters.capital.service */
-public class EmployeeService {
-}
